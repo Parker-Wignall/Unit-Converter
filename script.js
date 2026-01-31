f7b0ed24711bf3013b407c2c17b2ee5252c1032e
Parker-Wignall <parkerw444@gmail.com>
2026-01-30 18:27:26 -0800

    Add files via upload
    Basic HTML code for the front end, created multiple webpages to flip between for different conversion types, 
    
    Index -> converting between length measurements
    Weight -> converting between weight measurements
    Temperature -> converting between temperature measurements

---
+++ script.js
@@ -0,0 +1,5 @@
+      let convertLength = document.getElementById("box1").value;
+        let fromUnit = document.getElementById("box2").value;
+        let toUnit = document.getElementById("box3").value;
+
+        console.log(length, fromUnit, toUnit);
